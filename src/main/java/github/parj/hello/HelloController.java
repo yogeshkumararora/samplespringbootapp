df6d395be4d7e69d44be020e744dc8026655f025
Parjanya Mudunuri <parjanya@gmail.com>
2019-02-24 10:26:57 +0000

    #1 | Initial commit

---
+++ src/main/java/github/parj/hello/HelloController.java
@@ -0,0 +1,16 @@
+package github.parj.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public RedirectView redirectWithUsingRedirectView(RedirectAttributes attributes) {
+        return new RedirectView("swagger-ui.html");
+    }
+
+}
