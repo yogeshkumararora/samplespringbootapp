b714666db8c6a164272d6dd44b836418a8350822
Parjanya Mudunuri <parjanya@gmail.com>
2019-09-04 19:27:58 +0100

    Added in logging to stdout

---
+++ src/main/java/io/github/parj/hello/MicrometerConfig.java
@@ -0,0 +1,23 @@
+package io.github.parj.hello;
+
+//FROM https://github.com/izeye/sample-micrometer-spring-boot/blob/micrometer-gh-1431/src/main/java/com/izeye/sample/config/MicrometerConfig.java
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
+
+/**
+ * Configuration for Micrometer.
+ *
+ * @author Johnny Lim
+ */
+@Configuration
+public class MicrometerConfig {
+
+    @Bean
+    public LoggingMeterRegistry loggingMeterRegistry() {
+        return new LoggingMeterRegistry();
+    }
+
+}
